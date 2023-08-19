47fcb5a6a5b345e0fd134578a92800231b2091be
SalmonC <1518658371@qq.com>
2023-08-19 10:33:58 +0800

    AdminLogin.js地址修改

--- js/AdminLogin.js
+++ js/AdminLogin.js
@@ -4,22 +4,27 @@ function login() {
     
     /* if (username === 'admin' && password === '123') {
         alert('登录成功');
-        window.location.href = 'shouye.html';
+        window.location.href = '../layuiAdmin/layuiAdmin.v1.2.1 std/views/index.html';
     } else {
         alert('登录失败！请检查你的账号或密码。');
-    } */
+    } }*/
+
     if (username === 'admin'){
         if(password === '123'){
             alert('登录成功');
-            window.location.href = '../layuiAdmin/views/index.html';
-        }else if(password == 0){
+            window.location.href = '../layuiAdmin/layuiAdmin.v1.2.1 std/views/index.html';
+        }
+        else if(password == 0){
             alert('请输入您的密码！');
-        }else{
+        }
+        else{
             alert('密码输入错误！请检查您的密码。');
         }
-    }else if(username == 0){
+    }
+    else if(username == 0){
         alert('请输入您的账号！');
-    }else{
+    }
+    else{
         alert('账号输入错误！请检查您的账号。');
     }
 }
