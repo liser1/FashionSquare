161491561d4653743e3bd70b27dcb18e57fc138d
SalmonC <1518658371@qq.com>
2023-08-26 19:46:43 +0800

    修改管理员控制台

--- layuiAdmin/layuiAdmin.v1.2.1 std/layuiadmin/json/console/top-card.js
+++ layuiAdmin/layuiAdmin.v1.2.1 std/layuiadmin/json/console/top-card.js
@@ -4,70 +4,70 @@
   ,"count": "100"
   ,"data": [{
     "id": 111
-    ,"title": "社区开始接受 “赞助商广告” 投放"
-    ,"username": "贤心"
+    ,"title": "赞助商征集"
+    ,"username": "管理员王某"
     ,"channel": "公告"
     ,"href": "http://fly.layui.com/jie/15697/"
     ,"crt": 61632
   },{
     "id": 222
-    ,"title": "layui 一周年"
+    ,"title": "漫步时尚广场一周年"
     ,"username": "猫吃"
     ,"channel": "讨论"
     ,"href": "http://fly.layui.com/jie/16622/"
     ,"crt": 61632
   },{
     "id": 333
-    ,"title": "四个月的前端"
+    ,"title": "这件衣服好看吗家人们"
     ,"username": "fd"
     ,"channel": "分享"
     ,"href": "http://fly.layui.com/jie/16651/"
     ,"crt": 61632
   },{
     "id": 333
-    ,"title": "如何评价LayUI和他的作者闲心"
+    ,"title": "如何评价网站的分类管理"
     ,"username": "纸飞机"
     ,"channel": "提问"
     ,"href": "http://fly.layui.com/jie/9352/"
     ,"crt": 61632
   },{
     "id": 333
-    ,"title": "如何评价LayUI和他的作者闲心"
+    ,"title": "这件衣服是正品吗"
     ,"username": "纸飞机"
     ,"channel": "提问"
     ,"href": "http://fly.layui.com/jie/9352/"
     ,"crt": 61632
   },{
     "id": 333
-    ,"title": "如何评价LayUI和他的作者闲心"
+    ,"title": "如何评价网站运维团队"
     ,"username": "纸飞机"
     ,"channel": "提问"
     ,"href": "http://fly.layui.com/jie/9352/"
     ,"crt": 61632
   },{
     "id": 333
-    ,"title": "如何评价LayUI和他的作者闲心"
+    ,"title": "女朋友不喜欢，我该怎么办"
     ,"username": "纸飞机"
     ,"channel": "提问"
     ,"href": "http://fly.layui.com/jie/9352/"
     ,"crt": 61632
   },{
     "id": 333
-    ,"title": "如何评价LayUI和他的作者闲心"
+    ,"title": "男朋友过生日，我买什么好"
     ,"username": "纸飞机"
     ,"channel": "提问"
     ,"href": "http://fly.layui.com/jie/9352/"
     ,"crt": 61632
   },{
     "id": 333
-    ,"title": "如何评价LayUI和他的作者闲心"
+    ,"title": "有这样的弟弟你几点回家"
     ,"username": "纸飞机"
     ,"channel": "提问"
     ,"href": "http://fly.layui.com/jie/9352/"
     ,"crt": 61632
   },{
     "id": 333
-    ,"title": "如何评价LayUI和他的作者闲心"
+    ,"title": "哈哈哈哈哈"
     ,"username": "纸飞机"
     ,"channel": "提问"
     ,"href": "http://fly.layui.com/jie/9352/"
--- layuiAdmin/layuiAdmin.v1.2.1 std/layuiadmin/json/console/top-search.js
+++ layuiAdmin/layuiAdmin.v1.2.1 std/layuiadmin/json/console/top-search.js
@@ -3,35 +3,35 @@
   ,"msg": ""
   ,"count": "100"
   ,"data": [{
-    "keywords": "贤心是男是女"
+    "keywords": "亚麻超舒服vintage flat cap帅气男女贝雷前进帽英伦复古侦探帽子"
     ,"frequency": 8520
     ,"userNums": 2216
   },{
-    "keywords": "Java程序员能找到女朋友吗"
+    "keywords": "马思唯同款T恤"
     ,"frequency": 666
     ,"userNums": 333
   },{
-    "keywords": "此表格是静态模拟数据"
+    "keywords": "日系复古Vintage工装字母W贴布鸭舌帽女美式软顶休闲遮阳棒球帽男"
     ,"frequency": 666
     ,"userNums": 333
   },{
-    "keywords": "layui官方教程"
+    "keywords": "Skechers斯凯奇奶茶熊 秋季新款男女鞋海盐熊猫鞋休闲运动老爹鞋"
     ,"frequency": 666
     ,"userNums": 333
   },{
-    "keywords": "layui官方教程"
+    "keywords": "Nike耐克女鞋FLEX PLUS 2大童鞋夏新款透气网眼跑步鞋FJ7722-141"
     ,"frequency": 666
     ,"userNums": 333
   },{
-    "keywords": "layui官方教程"
+    "keywords": "潘婷氨基酸无硅油洗发水洗发露530g男女士去油蓬松净澈赋能"
     ,"frequency": 666
     ,"userNums": 333
   },{
-    "keywords": "layui官方教程"
+    "keywords": "华晨宇同款李宁跑步鞋吾适5S3.0男鞋新款夏透气减震跑鞋运动鞋男"
     ,"frequency": 666
     ,"userNums": 333
   },{
-    "keywords": "layui官方教程"
+    "keywords": "李宁跑步鞋男鞋休闲鞋透气复古老爹鞋跑鞋男士潮流百搭时尚运动鞋"
     ,"frequency": 666
     ,"userNums": 333
   },{
