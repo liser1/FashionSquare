ad680004b01923e171b3f04cdc038447f48552e3
SalmonC <1518658371@qq.com>
2023-08-26 16:47:49 +0800

    添加商品评价页面并修改评价内容

--- layuiAdmin/layuiAdmin.v1.2.1 std/layuiadmin/json/content/comment.js
+++ layuiAdmin/layuiAdmin.v1.2.1 std/layuiadmin/json/content/comment.js
@@ -5,52 +5,52 @@
   ,"data": [{
     "id": "001"
     ,"reviewers": "赵"
-    ,"content": "我又爱上编程了"
-    ,"commtime": 20160312
+    ,"content": "这裙子太好看了"
+    ,"commtime": 20230312
   },{
     "id": "002"
     ,"reviewers": "钱"
-    ,"content": "女生出门要小心"
-    ,"commtime": 20160821
+    ,"content": "太百搭了"
+    ,"commtime": 20230821
   },{
     "id": "003"
     ,"reviewers": "孙"
-    ,"content": "框架就用layui"
-    ,"commtime": 20161212
+    ,"content": "不错的，我喜欢"
+    ,"commtime": 20231212
   },{
     "id": "004"
     ,"reviewers": "李"
-    ,"content": "心姐么么哒" 
-    ,"commtime": 20170311
+    ,"content": "已经是回头客了"
+    ,"commtime": 20230311
   },{
     "id": "005"
     ,"reviewers": "周"
-    ,"content": "希望明天是个好天气"
-    ,"commtime": 20170612
+    ,"content": "女朋友很喜欢"
+    ,"commtime": 20230612
   },{
     "id": "006"
     ,"reviewers": "吴"
-    ,"content": "我又爱上编程了"
-    ,"commtime": 20171112
+    ,"content": "室友说很好看"
+    ,"commtime": 20231112
   },{
     "id": "007"
     ,"reviewers": "郑"
-    ,"content": "女生出门要小心"
-    ,"commtime": 20171230
+    ,"content": "我的尺码好像对不上"
+    ,"commtime": 20230730
   },{
     "id": "008"
     ,"reviewers": "王"
-    ,"content": "框架就用layui"
-    ,"commtime": 20180112
+    ,"content": "等到打折我再来买"
+    ,"commtime": 20230712
   },{
     "id": "009"
     ,"reviewers": "冯"
-    ,"content": "心姐么么哒"
-    ,"commtime": 20180221
+    ,"content": "是正品"
+    ,"commtime": 20230221
   },{
     "id": "010"
     ,"reviewers": "陈"
-    ,"content": "希望明天是个好天气"
-    ,"commtime": 20180312
+    ,"content": "客服服务态度很好"
+    ,"commtime": 20230312
   }]
 }
